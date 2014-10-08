9976d281c2e0dab00039e817a72ce31a70587bf0
David Guajardo <david@multidev.cl>
2014-10-08 16:58:59 -0300

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiblogApp::Application.config.secret_token = 'e2584a0e521c208d98d4ad08074cf1281b054e4429c3c39e19d24e5b82b8a7e7e2723c3b6922c436fda4615549ca1b1077683f082ee193f6bbd578949eabe34b'
